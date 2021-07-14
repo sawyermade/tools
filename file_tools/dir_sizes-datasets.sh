943496e9226c36caa6922979751e7ba51a518c90
sawyermade <sawyermade0@gmail.com>
2021-07-14 15:57:53 -0400

    lz

--- file_tools/dir_sizes-datasets.sh
+++ file_tools/dir_sizes-datasets.sh
@@ -10,20 +10,17 @@ echo "File/Directory sizes for directory: $1" | tee sizes.txt
 for dir in $dirs
 do
 	# Appends to sizes.txt
-<<<<<<< HEAD
 	# sudo du -sh $1/$dir | tee -a sizes.txt
 	#if ! [ "$dir" == "DATASETS" ]
 	#then
-=======
-	if [ "$dir" == "DATASETS" ]
-	then
-		echo -e "\nProcessing: $1/$dir"
-		sudo du -sh $1/$dir | tee -a DATASETS.txt
-	fi
+	#if [ "$dir" == "DATASETS" ]
+	#then
+	#	echo -e "\nProcessing: $1/$dir"
+	#	sudo du -sh $1/$dir | tee -a DATASETS.txt
+	#fi
 
-	if ! [ "$dir" == "DATASETS" ]
-	then
->>>>>>> ed561f2f7257c978ba8d8a1bcfe53f0bb6a67b20
+	#if ! [ "$dir" == "DATASETS" ]
+	#then
 		echo -e "\nProcessing: $1/$dir"
 		sudo du -sh $1/$dir | tee -a sizes.txt
 	#fi
